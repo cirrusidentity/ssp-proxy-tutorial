ef50b1eafe12fc0994e0853e02f0d8e69689f46f
Patrick Radtke <patrick@cirrusidentity.com>
2017-09-18 14:03:15 -0700

    Proxy: beginning config

---
+++ 3_Proxy_Setup/proxy/config/authsources.php
@@ -0,0 +1,28 @@
+<?php
+
+$config = array(
+
+    // This is a authentication source which handles admin authentication.
+    'admin' => array(
+        // The default is to use core:AdminPassword, but it can be replaced with
+        // any authentication source.
+
+        'core:AdminPassword',
+    ),
+
+    /* Easy way to authenticate users for a demo */
+/*
+    'tutorial-idp' => array(
+        'exampleauth:UserPass',
+        // Format is  username:passwor => array( attributes )
+        'student:studentpass' => array(
+            'uid' => array('test'),
+            'eduPersonAffiliation' => array('member', 'student'),
+        ),
+        'employee:employeepass' => array(
+            'uid' => array('employee'),
+            'eduPersonAffiliation' => array('member', 'employee'),
+        ),
+    ),
+*/
+);
